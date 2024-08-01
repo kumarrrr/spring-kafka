9ac534eb29c204d95add0321454d0856bae7d714
Nirmal Kumar <er.kumarnirmal@gmail.com>
2024-08-01 18:12:00 +0530

    Topic creation on the broker

---
+++ src/main/java/com/kumar/spring/config/KafkaTopicConfig.java
@@ -0,0 +1,14 @@
+package com.kumar.spring.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+    @Bean
+    public NewTopic netflixTopics(){
+        return TopicBuilder.name("IRON_MAN1").build();
+    }
+}
