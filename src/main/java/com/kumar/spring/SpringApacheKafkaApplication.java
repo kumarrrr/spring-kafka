e267a7939d14736c5ab4c9d6f89c1902defe9ea6
Nirmal Kumar <er.kumarnirmal@gmail.com>
2024-08-01 16:58:55 +0530

    Setup Kafka complete

---
+++ src/main/java/com/kumar/spring/SpringApacheKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kumar.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SpringApacheKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApacheKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kumar/spring/SpringApacheKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kumar.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApacheKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
